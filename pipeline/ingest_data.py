e371cf7e314e271546be104241a5906211423355
Baig <baigrahima81@gmail.com>
2026-01-20 08:35:57 +0000

    Update Dockerfile, ingest script, and compose file

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,24 +4,26 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm import tqdm
+import click
+
 
 dtype = {
     "VendorID":                  "Int64",
     "passenger_count":           "Int64",
-    "trip_distance":           "float64",
+    "trip_distance":             "float64",
     "RatecodeID":                "Int64",
-    "store_and_fwd_flag":       "string",
+    "store_and_fwd_flag":        "string",
     "PULocationID":              "Int64",
     "DOLocationID":              "Int64",
     "payment_type":              "Int64",
-    "fare_amount":             "float64",
-    "extra":                   "float64",
-    "mta_tax":                 "float64",
-    "tip_amount":              "float64",
-    "tolls_amount":            "float64",
-    "improvement_surcharge":   "float64",
-    "total_amount":            "float64",
-    "congestion_surcharge":    "float64"
+    "fare_amount":               "float64",
+    "extra":                     "float64",
+    "mta_tax":                   "float64",
+    "tip_amount":                "float64",
+    "tolls_amount":              "float64",
+    "improvement_surcharge":     "float64",
+    "total_amount":              "float64",
+    "congestion_surcharge":      "float64"
 }
 
 parse_dates = [
@@ -29,52 +31,55 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
+@click.command()
+@click.option('--user', default='root', help='PostgreSQL user')
+@click.option('--password', default='root', help='PostgreSQL password')
+@click.option('--host', default='localhost', help='PostgreSQL host')
+@click.option('--port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--table', default='yellow_taxi_data', help='Target table name')
+def ingest_data(user, password, host, port, db, table):
+    """
+    Ingest hardcoded NYC Yellow Taxi data (Jan 2021) into PostgreSQL in chunks.
+    """
+    year = 2021
+    month = 1
+    chunksize = 100000
 
-def run(year=2021,month=1):
-    pg_user = "root"
-    pg_pass = "root"
-    pg_host = "localhost"
-    pg_port = "5432"
-    pg_db = "ny_taxi"
+    url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    read_chunksize = 100000
+    # Connect to PostgreSQL
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
 
-    target_table = 'yellow_taxi_data'
-
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-
-    engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
-
+    # Read CSV in chunks
     df_iter = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=read_chunksize,
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
     )
 
     first_chunk = True
     for df_chunk in tqdm(df_iter):
         if first_chunk:
-            df.head(n=0).to_sql(
-                name=target_table,
-                con=engine, 
+            df_chunk.head(0).to_sql(
+                name=table,
+                con=engine,
                 if_exists='replace',
-                index= False
+                index=False
             )
             first_chunk = False
 
         df_chunk.to_sql(
-            name=target_table,
+            name=table,
             con=engine,
             if_exists='append',
             index=False
         )
-    print(f"Finished loading {target_table} for {year}-{month:02d}")
+
+    print(f"Finished loading {table} for {year}-{month:02d}")
 
 
 if __name__ == "__main__":
-    run(year=2021, month=1)
-
-
+    ingest_data()
